a1fd7b122d512cc2e9ec621546a8dc230a200e7f
Harduino <mail@harduino.com>
2018-06-16 11:48:01 +0300

    border-left-color :: emptying test file

--- test/test-docx-border-left-color.js
+++ test/test-docx-border-left-color.js
@@ -46,47 +46,7 @@ describe("DOCX generator", function () {
 		var docx = officegen ( 'docx' );
 		docx.on ( 'error', onError );
 
-		var pObj = docx.createP ();
-
-		pObj.addText ( 'Simple' );
-		pObj.addText ( ' with color', { color: '000088' } );
-		pObj.addText ( ' and back color.', { color: '00ffff', back: '000088' } );
-
-		var pObj = docx.createP ();
-
-		pObj.addText ( 'Bold + underline', { bold: true, underline: true } );
-
-		var pObj = docx.createP ( { align: 'center' } );
-
-		pObj.addText ( 'Center this text.' );
-
-		var pObj = docx.createP ();
-		pObj.options.align = 'right';
-
-		pObj.addText ( 'Align this text to the right.' );
-
-		var pObj = docx.createP ();
-
-		pObj.addText ( 'Those two lines are in the same paragraph,' );
-		pObj.addLineBreak ();
-		pObj.addText ( 'but they are separated by a line break.' );
-
-		docx.putPageBreak ();
-
-		var pObj = docx.createP ();
-
-		pObj.addText ( 'Fonts face only.', { font_face: 'Arial' } );
-		pObj.addText ( ' Fonts face and size.', { font_face: 'Arial', font_size: 40 } );
-
-		docx.putPageBreak ();
-
-		var pObj = docx.createListOfNumbers ();
-
-		pObj.addText ( 'Option 1' );
-
-		var pObj = docx.createListOfNumbers ();
-
-		pObj.addText ( 'Option 2' );
+		
 
 		var FILENAME = "test-docx-border-top.docx";
 		var out = fs.createWriteStream(OUTDIR + FILENAME);
